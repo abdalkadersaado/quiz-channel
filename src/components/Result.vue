dad2bb0a6d7886fa4bb7cecf7540a1e2ba02e1d4
abdalkader_saado <abdalkader1994953@gmail.com>
2024-11-13 11:31:01 +0300

    upload files

---
+++ src/components/Result.vue
@@ -0,0 +1,31 @@
+<script setup>
+import { computed } from "vue";
+const props = defineProps(['results', 'totalCorrect']);
+
+const resultIndex = computed(() => {
+    let index = 0;
+
+    props.results.forEach((e, i) => {
+        if (e.min <= props.totalCorrect && e.max >= props.totalCorrect) {
+            index = i;
+        }
+    });
+
+    return index;
+})
+</script>
+
+<template>
+    <div class="result">
+        <div class="title">{{results[resultIndex].title}}</div>
+        <div class="desc">
+            {{results[resultIndex].desc}}
+        </div>
+    </div>
+</template>
+
+
+
+<style>
+
+</style>
